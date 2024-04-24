c2cc5865ea52651fac82964fe764586fc15ef7a0
Matheus Pereira Martins <matheuspereiramartins1993@gmail.com>
2024-04-24 11:51:04 -0300

    second-commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -17,7 +17,7 @@ public class ContaTerminal {
         double saldo = scanner.nextDouble();
 
         System.out.printf(
-                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.\n",
+                "\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.\n",
                 nomeCliente, agencia, numero, saldo);
     }
 }
